dda69767211e6b89d46e6e48d86d9e8a09eec343
SamLua42 <bea.valenciahorna@gmail.com>
2026-07-29 02:44:42 -0500

    feat: agregar clase ConexionBD para conexion a BD_EduControl

---
+++ src/main/java/util/ConexionBD.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/BD_EduControl";
+	private static final String USER = "root";
+	private static final String PASSWORD = "Tam181113";
+	
+	
+	public static Connection miConexion()
+	{
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(URL,USER,PASSWORD);
+			}
+		catch (SQLException e) {
+			System.out.println("Error al conectar a la BD_EduControl" + e.getMessage());
+			e.printStackTrace();
+			}
+		
+		return con;
+	}
+	
+
+}
