3ed3b65d1805964c32ca7b00645344ff18c715b1
SamLua42 <bea.valenciahorna@gmail.com>
2026-07-30 04:15:04 -0500

    feat: creacion de ConceptoPagoDAO

---
+++ src/main/java/dao/ConceptoPagoDAO.java
@@ -0,0 +1,133 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import interfaces.IConceptoPagoDAO;
+import entidad.ConceptoPago;
+import util.ConexionBD;
+
+
+public class ConceptoPagoDAO implements IConceptoPagoDAO {
+
+	@Override
+	public void insertar(ConceptoPago c)
+	{
+		String sql = "INSERT INTO ConceptoPago(tipoPersonal, tarifaDiaria, descuentoTardanza, descuentoFalta) VALUES(?,?,?,?)";
+		
+		try (Connection con = ConexionBD.miConexion();
+			 PreparedStatement ps = con.prepareStatement(sql))
+		{
+			ps.setString(1, c.getTipoPersonal());
+			ps.setBigDecimal(2, c.getTarifaDiaria());
+			ps.setBigDecimal(3, c.getDescuentoTardanza());
+			ps.setBigDecimal(4, c.getDescuentoFalta());
+			ps.executeUpdate();
+		}
+		
+		catch (SQLException e) { e.printStackTrace(); }
+	}
+
+	
+	
+	@Override
+	public ConceptoPago buscarPorId(int idConcepto)
+	{
+		ConceptoPago c = null;
+		String sql = "SELECT * FROM ConceptoPago WHERE IDConcepto=?";
+		
+		try (Connection con = ConexionBD.miConexion();
+			 PreparedStatement ps = con.prepareStatement(sql))
+		{
+			ps.setInt(1, idConcepto);
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next())
+			{
+				c = new ConceptoPago();
+				c.setIdConcepto(rs.getInt("IDConcepto"));
+				c.setTipoPersonal(rs.getString("tipoPersonal"));
+				c.setTarifaDiaria(rs.getBigDecimal("tarifaDiaria"));
+				c.setDescuentoTardanza(rs.getBigDecimal("descuentoTardanza"));
+				c.setDescuentoFalta(rs.getBigDecimal("descuentoFalta"));
+			}
+		}
+		
+		catch (SQLException e) { e.printStackTrace();}
+		
+		return c;
+	}
+
+	
+	
+	@Override
+	public List<ConceptoPago> listar()
+	{
+		List<ConceptoPago> lista = new ArrayList<>();
+		String sql = "SELECT * FROM ConceptoPago";
+		
+		try (Connection con = ConexionBD.miConexion();
+			 PreparedStatement ps = con.prepareStatement(sql);
+		     ResultSet rs = ps.executeQuery())
+		{
+			while(rs.next())
+			{
+				ConceptoPago c = new ConceptoPago();
+				c.setIdConcepto(rs.getInt("IDConcepto"));
+				c.setTipoPersonal(rs.getString("tipoPersonal"));
+				c.setTarifaDiaria(rs.getBigDecimal("tarifaDiaria"));
+				c.setDescuentoTardanza(rs.getBigDecimal("descuentoTardanza"));
+				c.setDescuentoFalta(rs.getBigDecimal("descuentoFalta"));
+				lista.add(c);
+			}
+		}
+		
+		catch (SQLException e) {e.printStackTrace();}
+
+		return lista;
+	}
+
+	
+	
+	@Override
+	public void actualizar(ConceptoPago c) 
+	{
+		String sql = "UPDATE ConceptoPago SET tipoPersonal=?, tarifaDiaria=?, descuentoTardanza=?, descuentoFalta=? WHERE IDConcepto=?";
+		
+		try (Connection con = ConexionBD.miConexion();
+			 PreparedStatement ps = con.prepareStatement(sql))
+		{
+			ps.setString(1, c.getTipoPersonal());
+			ps.setBigDecimal(2, c.getTarifaDiaria());
+			ps.setBigDecimal(3, c.getDescuentoTardanza());
+			ps.setBigDecimal(4, c.getDescuentoFalta());
+			ps.setInt(5, c.getIdConcepto());
+			ps.executeUpdate();
+		}
+		
+		catch (SQLException e) {e.printStackTrace();}
+	}
+
+	
+	
+	@Override
+	public void eliminar(int idConcepto)
+	{
+		String sql = "DELETE FROM ConceptoPago WHERE IDConcepto=?";
+		
+		try (Connection con = ConexionBD.miConexion();
+			 PreparedStatement ps = con.prepareStatement(sql))
+		{
+			ps.setInt(1, idConcepto);
+			ps.executeUpdate();
+		}
+		
+		catch (SQLException e) {e.printStackTrace();}
+	}
+
+}
