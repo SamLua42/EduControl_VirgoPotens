8e472db0284ca0030c99a8567bd5f7e25a0bdb05
SamLua42 <bea.valenciahorna@gmail.com>
2026-07-30 05:47:22 -0500

    fix: agrego codigo en PersonalDAO

--- src/main/java/dao/PersonalDAO.java
+++ src/main/java/dao/PersonalDAO.java
@@ -15,34 +15,40 @@ import util.ConexionBD;
 public class PersonalDAO implements IPersonalDAO {
 
 	@Override
-	public void insertar(Personal p) 
+	public void insertar(Personal p)
 	{
-		String sql = "INSERT INTO Personal (nombre,apellido,dni) VALUES (?,?,?)";
+		String sql = "INSERT INTO Personal(nombre, apellido, dni, cargo, tipoPersonal, horaEntradaEsperada, usuario, contrasena, rol) VALUES(?,?,?,?,?,?,?,?,?)";
 		try (Connection con = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql))
-		{	
+		{
 			 ps.setString(1, p.getNombre());
 			 ps.setString(2, p.getApellido());
 			 ps.setString(3, p.getDni());
+			 ps.setString(4, p.getCargo());
+			 ps.setString(5, p.getTipoPersonal());
+			 ps.setTime(6, p.getHoraEntradaEsperada());
+			 ps.setString(7, p.getUsuario());
+			 ps.setString(8, p.getContrasena());
+			 ps.setString(9, p.getRol());
 			 ps.executeUpdate();
 		}
-		
+
 		catch (SQLException e) {e.printStackTrace();}
 	}
-	
 
-	
+
+
 	@Override
-	public Personal buscarPorId(int idPersonal) 
+	public Personal buscarPorId(int idPersonal)
 	{
 		Personal p = null;
-		String sql = "SELECT * FROM Personal WHERE idPersonal = ?";
+		String sql = "SELECT * FROM Personal WHERE IDPersonal = ?";
 		try (Connection con = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql))
 		{
 			 ps.setInt(1, idPersonal);
 			 ResultSet rs = ps.executeQuery();
-			 
+
 			 if(rs.next())
 			 {
 				 p = new Personal();
@@ -50,22 +56,30 @@ public class PersonalDAO implements IPersonalDAO {
 				 p.setNombre(rs.getString("nombre"));
 				 p.setApellido(rs.getString("apellido"));
 				 p.setDni(rs.getString("dni"));
-			 }	
+				 p.setCargo(rs.getString("cargo"));
+				 p.setTipoPersonal(rs.getString("tipoPersonal"));
+				 p.setHoraEntradaEsperada(rs.getTime("horaEntradaEsperada"));
+				 p.setUsuario(rs.getString("usuario"));
+				 p.setContrasena(rs.getString("contrasena"));
+				 p.setRol(rs.getString("rol"));
+				 p.setEstado(rs.getBoolean("estado"));
+				 p.setFechaRegistro(rs.getTimestamp("fechaRegistro"));
+			 }
 		}
-	
+
 		catch (SQLException e) {e.printStackTrace();}
-		
+
 		return p;
 	}
 
-	
-	
+
+
 	@Override
-	public List<Personal> listar() 
+	public List<Personal> listar()
 	{
 		List<Personal> lista = new ArrayList<>();
 		String sql = "SELECT * FROM Personal";
-		
+
 		try (Connection con = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql);
 			 ResultSet rs = ps.executeQuery())
@@ -73,54 +87,68 @@ public class PersonalDAO implements IPersonalDAO {
 			while(rs.next())
 			{
 				Personal p = new Personal();
-				p.setIdPersonal(rs.getInt("idPersonal"));
+				p.setIdPersonal(rs.getInt("IDPersonal"));
 				p.setNombre(rs.getString("nombre"));
 				p.setApellido(rs.getString("apellido"));
 				p.setDni(rs.getString("dni"));
+				p.setCargo(rs.getString("cargo"));
+				p.setTipoPersonal(rs.getString("tipoPersonal"));
+				p.setHoraEntradaEsperada(rs.getTime("horaEntradaEsperada"));
+				p.setUsuario(rs.getString("usuario"));
+				p.setContrasena(rs.getString("contrasena"));
+				p.setRol(rs.getString("rol"));
+				p.setEstado(rs.getBoolean("estado"));
+				p.setFechaRegistro(rs.getTimestamp("fechaRegistro"));
 				lista.add(p);
 			}
 		}
-		
-		catch (SQLException e) { e.printStackTrace(); }
-		
+
+		catch (SQLException e) {e.printStackTrace();}
+
 		return lista;
 	}
-	
-	
+
+
 
 	@Override
-	public void actualizar(Personal p) 
-	{	
-		String sql = "UPDATE Personal SET nombre=?, apellido=?, dni=? WHERE IDPersonal=?";
-		
+	public void actualizar(Personal p)
+	{
+		String sql = "UPDATE Personal SET nombre=?, apellido=?, dni=?, cargo=?, tipoPersonal=?, horaEntradaEsperada=?, usuario=?, contrasena=?, rol=? WHERE IDPersonal=?";
+
 		try (Connection con  = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql))
 		{
 			ps.setString(1, p.getNombre());
 			ps.setString(2, p.getApellido());
 			ps.setString(3, p.getDni());
-			ps.setInt(4, p.getIdPersonal());
+			ps.setString(4, p.getCargo());
+			ps.setString(5, p.getTipoPersonal());
+			ps.setTime(6, p.getHoraEntradaEsperada());
+			ps.setString(7, p.getUsuario());
+			ps.setString(8, p.getContrasena());
+			ps.setString(9, p.getRol());
+			ps.setInt(10, p.getIdPersonal());
 			ps.executeUpdate();
 		}
-		
-		catch (SQLException e) { e.printStackTrace(); }
+
+		catch (SQLException e) {e.printStackTrace();}
 	}
 
-	
-	
+
+
 	@Override
-	public void eliminar(int idPersonal) 
+	public void eliminar(int idPersonal)
 	{
 		String sql = "DELETE FROM Personal WHERE IDPersonal=?";
-		
+
 		try (Connection con = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql))
 		{
 			ps.setInt(1, idPersonal);
 			ps.executeUpdate();
 		}
-		
-		catch (SQLException e) { e.printStackTrace(); }		
+
+		catch (SQLException e) {e.printStackTrace();}
 	}
-	
+
 }
