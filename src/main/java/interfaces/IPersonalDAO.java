9f57b170379d700db8e4659f6820fdc2635e9685
SamLua42 <bea.valenciahorna@gmail.com>
2026-08-03 16:09:55 -0500

    fix: se agrego metodo en IPersonalDAO

--- src/main/java/interfaces/IPersonalDAO.java
+++ src/main/java/interfaces/IPersonalDAO.java
@@ -6,6 +6,7 @@ import entidad.Personal;
 public interface IPersonalDAO {
 	void insertar(Personal p);
 	Personal buscarPorId(int idPersonal);
+	Personal buscarPorUsuario(String usuario);
 	List<Personal> listar();
 	void actualizar(Personal p);
 	void eliminar(int idPersonal);
