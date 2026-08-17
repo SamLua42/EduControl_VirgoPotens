b6116b9c60e58d602094a8b1e7748380f768e3ec
SamLua42 <bea.valenciahorna@gmail.com>
2026-08-17 14:36:01 -0500

    fix: correccion de DetallePlanillaDAO.java

--- src/main/java/dao/DetallePlanillaDAO.java
+++ src/main/java/dao/DetallePlanillaDAO.java
@@ -17,8 +17,8 @@ public class DetallePlanillaDAO implements IDetallePlanillaDAO {
 	@Override
 	public void insertar(DetallePlanilla dp)
 	{
-		String sql = "INSERT INTO DetallePlanilla(IDPlanilla, IDPersonal, diasTrabajados, diasTardanza, diasFalta, montoDescuento, montoTotal) VALUES(?,?,?,?,?,?,?)";
-		
+		String sql = "INSERT INTO DetallePlanilla(IDPlanilla, IDPersonal, diasTrabajados, diasTardanza, diasFalta, montoBruto, montoDescuento, montoDescuentoPension, montoEssalud, montoTotal, montoNeto) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+
 		try (Connection con = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql))
 		{
@@ -27,25 +27,31 @@ public class DetallePlanillaDAO implements IDetallePlanillaDAO {
 			ps.setInt(3, dp.getDiasTrabajados());
 			ps.setInt(4, dp.getDiasTardanza());
 			ps.setInt(5, dp.getDiasFalta());
-			ps.setBigDecimal(6, dp.getMontoDescuento());
-			ps.setBigDecimal(7, dp.getMontoTotal());
+			ps.setBigDecimal(6, dp.getMontoBruto());
+			ps.setBigDecimal(7, dp.getMontoDescuento());
+			ps.setBigDecimal(8, dp.getMontoDescuentoPension());
+			ps.setBigDecimal(9, dp.getMontoEssalud());
+			ps.setBigDecimal(10, dp.getMontoTotal());
+			ps.setBigDecimal(11, dp.getMontoNeto());
 			ps.executeUpdate();
 		}
 		catch (SQLException e) {e.printStackTrace();}
 	}
 
-	
-	
+
+
 	@Override
 	public DetallePlanilla buscarPorId(int idDetalle)
 	{
 		DetallePlanilla dp = null;
 		String sql = "SELECT * FROM DetallePlanilla WHERE IDDetalle=?";
-		
+
 		try (Connection con = ConexionBD.miConexion();
-			 PreparedStatement ps = con.prepareStatement(sql);
-			 ResultSet rs = ps.executeQuery())
+			 PreparedStatement ps = con.prepareStatement(sql))
 		{
+			ps.setInt(1, idDetalle);
+			ResultSet rs = ps.executeQuery();
+
 			if(rs.next())
 			{
 				dp = new DetallePlanilla();
@@ -55,24 +61,28 @@ public class DetallePlanillaDAO implements IDetallePlanillaDAO {
 				dp.setDiasTrabajados(rs.getInt("diasTrabajados"));
 				dp.setDiasTardanza(rs.getInt("diasTardanza"));
 				dp.setDiasFalta(rs.getInt("diasFalta"));
+				dp.setMontoBruto(rs.getBigDecimal("montoBruto"));
 				dp.setMontoDescuento(rs.getBigDecimal("montoDescuento"));
+				dp.setMontoDescuentoPension(rs.getBigDecimal("montoDescuentoPension"));
+				dp.setMontoEssalud(rs.getBigDecimal("montoEssalud"));
 				dp.setMontoTotal(rs.getBigDecimal("montoTotal"));
+				dp.setMontoNeto(rs.getBigDecimal("montoNeto"));
 			}
 		}
-		
+
 		catch (SQLException e) {e.printStackTrace();}
-		
+
 		return dp;
 	}
 
-	
-	
+
+
 	@Override
 	public List<DetallePlanilla> listar()
 	{
 		List<DetallePlanilla> lista = new ArrayList<>();
 		String sql = "SELECT * FROM DetallePlanilla";
-		
+
 		try (Connection con = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql);
 		     ResultSet  rs = ps.executeQuery())
@@ -86,24 +96,28 @@ public class DetallePlanillaDAO implements IDetallePlanillaDAO {
 				dp.setDiasTrabajados(rs.getInt("diasTrabajados"));
 				dp.setDiasTardanza(rs.getInt("diasTardanza"));
 				dp.setDiasFalta(rs.getInt("diasFalta"));
+				dp.setMontoBruto(rs.getBigDecimal("montoBruto"));
 				dp.setMontoDescuento(rs.getBigDecimal("montoDescuento"));
+				dp.setMontoDescuentoPension(rs.getBigDecimal("montoDescuentoPension"));
+				dp.setMontoEssalud(rs.getBigDecimal("montoEssalud"));
 				dp.setMontoTotal(rs.getBigDecimal("montoTotal"));
+				dp.setMontoNeto(rs.getBigDecimal("montoNeto"));
 				lista.add(dp);
 			}
 		}
-		
+
 		catch (SQLException e) {e.printStackTrace();}
-		
+
 		return lista;
 	}
 
-	
-	
+
+
 	@Override
 	public void actualizar(DetallePlanilla dp)
 	{
-		String sql = "UPDATE DetallePlanilla SET IDPlanilla=?, IDPersonal=?, diasTrabajados=?, diasTardanza=?, diasFalta=?, montoDescuento=?, montoTotal=? WHERE IDDetalle=?";
-		
+		String sql = "UPDATE DetallePlanilla SET IDPlanilla=?, IDPersonal=?, diasTrabajados=?, diasTardanza=?, diasFalta=?, montoBruto=?, montoDescuento=?, montoDescuentoPension=?, montoEssalud=?, montoTotal=?, montoNeto=? WHERE IDDetalle=?";
+
 		try (Connection con = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql))
 		{
@@ -112,29 +126,33 @@ public class DetallePlanillaDAO implements IDetallePlanillaDAO {
 			ps.setInt(3, dp.getDiasTrabajados());
 			ps.setInt(4, dp.getDiasTardanza());
 			ps.setInt(5, dp.getDiasFalta());
-			ps.setBigDecimal(6, dp.getMontoDescuento());
-			ps.setBigDecimal(7, dp.getMontoTotal());
-			ps.setInt(8, dp.getIdDetalle());
+			ps.setBigDecimal(6, dp.getMontoBruto());
+			ps.setBigDecimal(7, dp.getMontoDescuento());
+			ps.setBigDecimal(8, dp.getMontoDescuentoPension());
+			ps.setBigDecimal(9, dp.getMontoEssalud());
+			ps.setBigDecimal(10, dp.getMontoTotal());
+			ps.setBigDecimal(11, dp.getMontoNeto());
+			ps.setInt(12, dp.getIdDetalle());
 			ps.executeUpdate();
 		}
-		
-		catch (SQLException e) {e.printStackTrace();}	
+
+		catch (SQLException e) {e.printStackTrace();}
 	}
 
-	
-	
+
+
 	@Override
 	public void eliminar(int idDetalle)
 	{
 		String sql = "DELETE FROM DetallePlanilla WHERE IDDetalle=?";
-		
+
 		try (Connection con = ConexionBD.miConexion();
 			 PreparedStatement ps = con.prepareStatement(sql))
-		{	
+		{
 			ps.setInt(1, idDetalle);
 			ps.executeUpdate();
 		}
-		
+
 		catch (SQLException e) {e.printStackTrace();}
 	}
 
